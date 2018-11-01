8a0fe363aae554bebf9094c629abe2b1bcd3bd7b
Rexman17 <a.stone1794@gmail.com>
2018-11-01 11:35:50 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  # route call
+  # creates routing methods for posts that we can utilize in our views and controllers
   resources :posts, only: [:index, :show]
 end
